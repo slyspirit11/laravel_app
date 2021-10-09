6ce1672ec8679e496a75bd146023049455dfb897
Emperor <batt098@gmail.com>
2021-10-10 01:04:56 +0500

    added reviews table migration script

---
+++ database/migrations/2021_10_09_190244_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('movie_id')->nullable()->default(null);
+            $table->date('date');
+            $table->text('content');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
